9f0c714f9b1221e00a7589fb8447aaff5139bc42
Mimadfaoussi <imadfaoussi@gmail.com>
2025-01-16 21:03:27 +0100

    automating alteration of mariadb config file inside the container using sed , and updating wordpress config file to create php config file

--- srcs/requirements/mariadb/conf/init_db.sh
+++ srcs/requirements/mariadb/conf/init_db.sh
@@ -7,6 +7,8 @@ if [ ! -d "$DB_DIR/mysql" ]; then
     mariadb-install-db --user=mysql --basedir=/usr --datadir="$DB_DIR"
 fi
 
+sed -i "s|bind-address\s*=\s*127.0.0.1|bind-address = 0.0.0.0|g" /etc/mysql/mariadb.conf.d/50-server.cnf
+
 mysqld_safe --datadir="$DB_DIR" &
 sleep 5
 
@@ -19,6 +21,7 @@ mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
 
 mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_ADMIN_USER}'@'%' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_ADMIN_USER}'@'%';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ADMIN_USER}'@'%' WITH GRANT OPTION;"
 
 mysql -e "FLUSH PRIVILEGES;"
 
